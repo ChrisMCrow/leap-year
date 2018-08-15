50b3831b3f65aa9c6f95a48bdfe1110c80b44d47
Chris Crow and Connor McCarthy <connor@>
2018-08-15 09:24:33 -0700

    indent code

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 var leapYear = function(year){
-if ((year % 400 === 0) || (year % 4 === 0) && (year % 100 != 0)){
-  return true;
-
-  }  else {
+  if ((year % 400 === 0) || (year % 4 === 0) && (year % 100 != 0)){
+    return true;
+  } else {
     return false;
-}
+  }
 };
 $(document).ready(function(){
   $("form#leap-year").submit(function(event){
